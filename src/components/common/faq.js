709e08580d22b129c802df0e5241fad07edd21be
david <davidealvarez1@gmail.com>
2021-02-12 15:02:36 -0800

    edited faq.js to reflect Labs31 LAN-B team

--- src/components/common/faq.js
+++ src/components/common/faq.js
@@ -8,18 +8,15 @@ const FAQ = props => {
             <Header history={props.history} />
             <FAQContainer>
                 <h2>Frequently Asked Questions</h2>
-                <p className='question'>What is viewee?</p>
-                <p className='answer'>viewee is a platform that allows Lambda School students to answer, view, and post job interview questions to prepare for the real thing.</p>
-                <p className='question'>Why viewee?</p>
-                <p className='answer'>I felt there wasn't a platform who's sole purpose was about interview questions. Often it was tacked on to job and interview preparation sites and not really given much focus.</p>
-                <p className='question'>Is it easy to get started with viewee?</p>
-                <p className='answer'>Extremely easy. You can register in seconds and immediately start viewing, answering, and posting questions.</p>
-                <p className='question'>Who built viewee?</p>
-                <p className='answer'><a href='https://github.com/miugel' target='_blank' rel='noopener noreferrer'>Miguel</a></p>
+                <p className='question'>What is LAN?</p>
+                <p className='answer'>LAN or Lambda Alumni Network is a unified, private community for Lambda school alumni to connect, advance their careers, and give back.</p>
+                <p className='question'>Why LAN?</p>
+                <p className='answer'>LAN will provide communication on important factors for alumni in endorsement and outside of Lambda School, such as: forums, job opportunities, interview prep,
+                events(hackathons, conferences, speaking opportunities etc.), and Personal finance resources</p>
+                <p className='question'>Who built LAN?</p>
+                <p className='answer'><a href='https://github.com/orgs/Lambda-School-Labs/teams/labs-31-lambda-alumni-network-b' target='_blank' rel='noopener noreferrer'>Labs31 LAN B team</a></p>
                 <p className='question'>What tech stack are you using?</p>
                 <p className='answer'>React and styled-components on the front end and Express and Postgres on the back end.</p>
-                <p className='question'>How can I get involved?</p>
-                <p className='answer'>You can fork the repository, make changes, and submit a pull request. If the changes are satisfactory, I will merge!</p>
             </FAQContainer>
         </>
     );
