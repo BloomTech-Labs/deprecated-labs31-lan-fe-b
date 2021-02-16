921bdc420154a282e017959c3835f84678943e0b
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2021-02-16 09:15:04 -0800

    fixed issue when you went onto editroles and tried to redirect back to some other page it would crash

--- src/components/admin/editRoles.js
+++ src/components/admin/editRoles.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { updateUserDisplayName } from '../../actions';
 import Header from '../common/header';
 
-const editRoles = () => {
+const editRoles = props => {
     return (
         <>
-            <Header />
+            <Header history={props.history}/>
             <h1>Manage User Roles Page</h1>
         </>
     )
 }
 
-export default editRoles
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    };
+};
+
+export default connect(mapStateToProps, { updateUserDisplayName })(editRoles);
