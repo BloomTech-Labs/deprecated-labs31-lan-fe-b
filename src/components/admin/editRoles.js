f00f58589bd9fe369d27e605073351766b507f81
Matthew Serwer <Matthew.Serwer@gmail.com>
2021-02-16 18:55:44 -0500

    Creates createRole component. Form is complete. No styles are applied. Creates admin/styles folder.

--- src/components/admin/editRoles.js
+++ src/components/admin/editRoles.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Header from '../common/header';
+import CreateRole from './createRole';
 
 const editRoles = props => {
     return (
         <>
             <Header history={props.history}/>
             <h1>Manage User Roles Page</h1>
+            <CreateRole />
         </>
     )
 }
