0475da2ab59018142c89a70e97e6395268f8de14
david <davidealvarez1@gmail.com>
2021-02-24 10:27:11 -0800

    removed private route

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
       <PrivateRoute path="/error" component={Error} />
       <PrivateRoute path="/edit/roles" component={editRoles} />
       <PrivateRoute path="/edit/rooms" component={editRooms} />
-      <PrivateRoute path= "/edit/users" component={editUsers}/>
+      <Route path= "/edit/users" component={editUsers}/>
       <Route component={PageNotFound} />
     </Switch>
   );
