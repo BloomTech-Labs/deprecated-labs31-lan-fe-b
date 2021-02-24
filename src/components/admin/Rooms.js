0fdd3329523b3c0b1b5f670b95ff0b8583253b5c
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2021-02-24 11:54:37 -0800

    added a rooms component to reslolve issue of edit button where it would open all instead of each individual one when clicked

---
+++ src/components/admin/Rooms.js
@@ -0,0 +1,156 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const RoomsContainer = styled.div`
+  .rooms-card {
+    padding: 16px;
+    margin: 16px auto;
+    background-color: #2c2f33;
+    border-radius: 3px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    display: flex;
+    width: 52%;
+  }
+  .right {
+    .display-name-and-timestamp {
+      margin-bottom: 4px;
+      display: flex;
+      align-items: center;
+
+      .display-name {
+        margin-right: 8px;
+        padding-bottom: 18px;
+        font-size: 0.875rem;
+        font-weight: 500;
+        color: #ffffff;
+        transition: 0.25s;
+
+        :hover {
+          opacity: 0.5;
+        }
+      }
+
+      .timestamp {
+        font-size: 0.625rem;
+        font-weight: 500;
+        color: #ffffff;
+        padding-bottom: 16px;
+      }
+    }
+
+    .labels {
+      margin-bottom: 4px;
+      display: flex;
+    }
+
+    .description {
+      margin-bottom: 4px;
+      padding-bottom: 10px;
+      font-size: 1rem;
+      font-weight: 600;
+      color: #ffffff;
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+      width: 75%;
+    }
+    button {
+      padding: 10px 24px;
+      margin-top: 16px;
+      margin-left: 5px;
+      background: linear-gradient(to right, #0084ff, #0099ff);
+      border: none;
+      border-radius: 3px;
+      font-family: 'Nunito', sans-serif;
+      font-size: 0.875rem;
+      font-weight: 600;
+      color: #ffffff;
+      cursor: pointer;
+      transition: 0.25s;
+
+      :hover {
+        opacity: 0.5;
+      }
+    }
+  }
+  label {
+    display: flex;
+    flex-direction: column;
+    font-size: 1rem;
+    font-weight: 500;
+    color: #ffffff;
+    width: 52%;
+  }
+
+  input {
+    width: 373px;
+    height: 50px;
+    padding: 10px;
+    background-color: #313536;
+    border: none;
+    border-radius: 3px;
+    font-family: 'Nunito', sans-serif;
+    font-size: 1rem;
+    font-weight: 500;
+    color: #ffffff;
+
+    ::placeholder {
+      color: dimgray;
+    }
+  }
+  textarea {
+    min-height: 44px;
+    width: 373px;
+    min-width: 373px;
+    max-width: 373px;
+    height: 140px;
+    padding: 10px;
+    background-color: #313536;
+    border: none;
+    border-radius: 3px;
+    font-family: 'Nunito', sans-serif;
+    font-size: 1rem;
+    font-weight: 500;
+    color: #ffffff;
+  }
+`;
+export const Rooms = (props) => {
+  const [updateForm, setUpdateForm] = useState(false);
+  return (
+    <>
+      <RoomsContainer>
+        <div className="rooms-card">
+          <div className="right">
+            <div className="display-name-and-timestamp">
+              <p className="display-name">{props.item.room}</p>
+              <p className="timestamp">{props.item.timestamp}</p>
+            </div>
+            <p className="description">{props.item.description}</p>
+            <button
+              onClick={() => {
+                setUpdateForm(!updateForm);
+              }}
+            >
+              Edit
+            </button>
+            {updateForm && (
+              <form>
+                <label>
+                  Update Room
+                  <input value={props.item.room} />
+                </label>
+                <label>
+                  Update Description
+                  <textarea value={props.item.description} />
+                </label>
+                <button>Update</button>
+              </form>
+            )}
+            <button>Delete</button>
+          </div>
+        </div>
+      </RoomsContainer>
+    </>
+  );
+};
--- src/components/admin/editRooms.js
+++ src/components/admin/editRooms.js
@@ -2,121 +2,7 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import Header from '../common/header';
 import CreateRooms from './createRooms';
-import styled from 'styled-components';
-
-const RoomsContainer = styled.div`
-  .rooms-card {
-    padding: 16px;
-    margin: 16px auto;
-    background-color: #2c2f33;
-    border-radius: 3px;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    display: flex;
-    width: 52%;
-  }
-  .right {
-    .display-name-and-timestamp {
-      margin-bottom: 4px;
-      display: flex;
-      align-items: center;
-
-      .display-name {
-        margin-right: 8px;
-        padding-bottom: 18px;
-        font-size: 0.875rem;
-        font-weight: 500;
-        color: #ffffff;
-        transition: 0.25s;
-
-        :hover {
-          opacity: 0.5;
-        }
-      }
-
-      .timestamp {
-        font-size: 0.625rem;
-        font-weight: 500;
-        color: #ffffff;
-        padding-bottom: 16px;
-      }
-    }
-
-    .labels {
-      margin-bottom: 4px;
-      display: flex;
-    }
-
-    .description {
-      margin-bottom: 4px;
-      padding-bottom: 10px;
-      font-size: 1rem;
-      font-weight: 600;
-      color: #ffffff;
-      display: -webkit-box;
-      -webkit-line-clamp: 2;
-      -webkit-box-orient: vertical;
-      overflow: hidden;
-      width: 75%;
-    }
-    button {
-      padding: 10px 24px;
-      margin-top: 16px;
-      margin-left: 5px;
-      background: linear-gradient(to right, #0084ff, #0099ff);
-      border: none;
-      border-radius: 3px;
-      font-family: 'Nunito', sans-serif;
-      font-size: 0.875rem;
-      font-weight: 600;
-      color: #ffffff;
-      cursor: pointer;
-      transition: 0.25s;
-
-      :hover {
-        opacity: 0.5;
-      }
-    }
-  }
-  label {
-    display: flex;
-    flex-direction: column;
-    /* margin: 16px auto 4px; */
-    font-size: 1rem;
-    font-weight: 500;
-    color: #ffffff;
-    width: 52%;
-  }
-
-  input {
-    padding: 10px;
-    background-color: #313536;
-    border: none;
-    border-radius: 3px;
-    font-family: 'Nunito', sans-serif;
-    font-size: 1rem;
-    font-weight: 500;
-    color: #ffffff;
-
-    ::placeholder {
-      color: dimgray;
-    }
-  }
-  textarea {
-    min-height: 44px;
-    width: 373px;
-    min-width: 373px;
-    max-width: 373px;
-    height: 140px;
-    padding: 10px;
-    background-color: #313536;
-    border: none;
-    border-radius: 3px;
-    font-family: 'Nunito', sans-serif;
-    font-size: 1rem;
-    font-weight: 500;
-    color: #ffffff;
-  }
-`;
+import { Rooms } from './Rooms';
 
 const roles_data = [
   {
@@ -143,44 +29,12 @@ const roles_data = [
 ];
 
 const EditRooms = (props) => {
-  const [updateForm, setUpdateForm] = useState(false);
   return (
     <>
       <Header history={props.history} />
       <CreateRooms />
       {roles_data.map((item) => (
-        <RoomsContainer key={item.id}>
-          <div className="rooms-card">
-            <div className="right">
-              <div className="display-name-and-timestamp">
-                <p className="display-name">{item.room}</p>
-                <p className="timestamp">{item.timestamp}</p>
-              </div>
-              <p className="description">{item.description}</p>
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  setUpdateForm(!updateForm);
-                }}
-              >
-                Edit
-              </button>
-              {updateForm && (
-                <form>
-                  <label>
-                    Update Room
-                    <input value={item.room} />
-                  </label>
-                  <label>
-                    Update Description
-                    <textarea value={item.description} />
-                  </label>
-                </form>
-              )}
-              <button>Delete</button>
-            </div>
-          </div>
-        </RoomsContainer>
+        <Rooms item={item} />
       ))}
     </>
   );
