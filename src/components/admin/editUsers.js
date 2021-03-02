c478d4c6b6f248a1371980dafeb0a39de9fea235
Yinusa <yamusa2@uic.edu>
2021-03-02 17:03:20 -0600

    finished request and form need to do redux

--- src/components/admin/editUsers.js
+++ src/components/admin/editUsers.js
@@ -4,19 +4,38 @@ import { connect } from 'react-redux';
 import Header from '../common/header';
 import { fetchUsers, fetchRoles } from '../../actions/index.js';
 import EditUserContainer from './styles/editRolesStyle';
+import axios from 'axios';
 import GetMyUsers from './getCreateUserRoom';
 
 
 const EditUsers = (props) => {
   const [successCount, setSuccessCount]= useState('')
+  const [values,setValues] = useState('')
+  const [name,setName] = useState('')
   useEffect(() => {
     props.fetchUsers();
   },[]);
   useEffect(()=>{
     props.fetchRoles();
-  },[]);
+  },[successCount]);
 
-  
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log("event.target.value,name: ",values,name)
+    axios
+      .put(`http://localhost:5000/api/user/${values}`,name)
+      .then(res => {
+        console.log("res: ", res.data)
+      })
+      .catch(err => console.log("err.response: ", err.response))
+  }
+
+  const handleChanges = (event) => {
+    event.preventDefault();
+    console.log("MY EVENTS",event.target,event.target.name)
+    setValues(event.target.value)
+    setName(event.target.name)
+  }
 
   return (
     <>
@@ -26,12 +45,31 @@ const EditUsers = (props) => {
       <Header history={props.history} />
       <EditUserContainer>
         {props.users.map(user => {
-         return <div>
+          console.log("THIS IS",user.id)
+         return (<div id={user.id}>
             <h1>{user.display_name}</h1>
             <img src={`${user.profile_picture}`} />
-            <GetMyUsers props={props}/>
+                    <form onSubmit={onSubmit}>
+                      <label>
+                        Choose A role:
+                        <select name={user.id} onChange={handleChanges}>
+                          {props.roles.map(roles=>{
+                            return(
+                            <option value={roles.id} name={user.id}>{roles.role_name}</option>
+                            )
+                          })}
+                        </select>
+                      </label> 
+
+                       <button type="submit">
+                    Submit
+                    </button>   
+                      </form> 
+            
           </div>
+          )
         })}
+        
       </EditUserContainer>
     </>
   );
--- src/components/admin/getCreateUserRoom.js
+++ src/components/admin/getCreateUserRoom.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import {connect} from 'react-redux';
 
 
@@ -6,28 +7,39 @@ import {connect} from 'react-redux';
 //purpose is to display a corresponding users role
 // this will take in a user id and then link itself to the role table
 const GetMyUsers = (props) => {
-    console.log(props)
-    console.log(props.props.roles.length)
-    const [count,setCount]= useState(0)
-    
+    //console.log(props.props.users)
+    //console.log(props.props.roles[0])
+    const [curUser,setCurUser]= useState(props.curUser)
+    const [roles,SetRoles] = useState(props.roles)
+    console.log("THIS IS IT",curUser,roles)
+// const testFunction = (event) => {
+//     event.preventDefault();
+//     console.log("event.target.value: ", event.target.value)
+//     axios
+//       .put(`http://localhost:5000/api/user/${event.target.value}`)
+//       .then(res => {
+//         console.log("res: ", res.data)
+//       })
+//       .catch(err => console.log("err.response: ", err.response))
+//   }
+
+  const onCheck = (event) => {
+      console.log(event)
+  }
+
+
     return(
         <form>
             <label>
                 Choose A Role:
                 <select>
-            {props.props.roles.map(role=> {
-                 {setCount(count+1)}
-             return(
-                
-                    <option value ={`${role.id}`} value2 = {`${props.props.roles[count]}`}> {role.role_name}  </option>
-                
-                
-             )
- 
-            })}
+                    <option value ={`${roles.id}`} value2 ={`${props.curUser}`} onChange={onCheck} > {roles.role_name}</option>
             </select>
             </label>
-            <input type="submit" value="Submit"/>
+               <button type="submit" value="submit">
+                    Submit
+                    </button>
+    
         </form>
 
     )
