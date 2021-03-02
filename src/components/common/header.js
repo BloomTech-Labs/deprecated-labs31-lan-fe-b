a2018dc28104da9ee22e5a551b86bedab95023dd
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2021-03-02 10:16:31 -0800

    removed extra p tag in header

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -65,7 +65,6 @@ const Header = (props) => {
           >
             Edit Rooms
           </p>
-          <p>Edit Rooms</p>
           <p
             onClick={() => {
               props.history.push('/edit/roles');
