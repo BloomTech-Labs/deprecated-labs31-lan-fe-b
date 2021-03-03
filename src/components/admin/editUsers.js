a71d1995ec732a97a36974ab3c467ad101aa91a9
david <davidealvarez1@gmail.com>
2021-03-03 08:21:30 -0800

    added onClick for axios put to edit user role

--- src/components/admin/editUsers.js
+++ src/components/admin/editUsers.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Header from '../common/header';
 import { fetchUsers, fetchRoles } from '../../actions';
 import EditUserContainer from './styles/editRolesStyle';
+import axios from "axios";
 
 const EditUsers = (props) => {
   console.log("props---->: ", props)
@@ -15,6 +16,18 @@ const EditUsers = (props) => {
     props.fetchRoles();
   },[]);
 
+  const onClick = (event) => {
+    event.preventDefault();
+    console.log("event.target: ", event.target)
+    console.log("props.roles[event.target.value2]: ", props.roles[event.target.value2])
+    axios
+      .put(`http://localhost:5000/api/user/${event.target.dataset.user_id}`, {role_id:event.target.value})
+      .then(res => {
+        console.log("res: ", res)
+      })
+      .catch(err => console.log("err.response: ", err.response))
+  }
+
   return (
     <>
       <Header history={props.history} />
@@ -23,13 +36,16 @@ const EditUsers = (props) => {
             return(
               <>
               <h1>{user.email}</h1>
-              <form>
-              <select>
+              <p>{user.id}</p>
+              <form onClick={onClick}>
+              <select data-user_id={user.id}>
               {
                 props.roles.map(role => {//get all roles
-                // <h1>{role.role_name}</h1>
+                <h1>{role.role_name}</h1>
                 return(
-                <option>{role.role_name}</option>
+                  <>
+                  <option value={role.id}>{role.role_name}</option>
+                  </>
                 )
               })
               }
